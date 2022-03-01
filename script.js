b73118538217c8e0ac14dc88498cad994c1b7f5d
t-creates <76760849+t-creates@users.noreply.github.com>
2022-02-28 19:34:16 -0700

    Adding NPM & JSON

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-import { without } from 'lodash';
+var _ = require('lodash');
 
-
-console.log(without);
+let array = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log('answer: ', _.without(array, 3));
 var css = document.querySelector('h3');
 var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 var body = document.getElementById('gradient');
 
 function setGradient() {
-	body.style.background = 'linear-gradient(to right, ' 
-	+ color1.value 
-	+ ', ' 
-	+ color2.value 
-	+ ')';
+	body.style.background = 'linear-gradient(to right, '
+		+ color1.value
+		+ ', '
+		+ color2.value
+		+ ')';
 
 	css.textContent = body.style.background + ';';
 }
